d596be04cec04d15c8493a6c3c90e2ea46ac94ce
Martin Citoler Saumell <martin.citoler@bigblueanalytics.com>
2019-06-13 10:21:21 +0200

    added landscaping.pyx example

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
-
+# from distutils.core import setup
+#
 from setuptools import Extension, setup
 
 ext_modules = [
-    Extension(name='fib', sources=['fib.pyx', 'cfib.c'], include_dirs=['.']),
+    # Extension(name="fib", sources=["fib.pyx", "cfib.c"], include_dirs=["."])
+    Extension(name="shrubbing", sources=["shrubbing.pyx"], language="c++"),
+    Extension(name="landscaping", sources=["landscaping.pyx"], language="c++"),
 ]
 
-setup(
-    name='cython_helloworld',
-    ext_modules=ext_modules
-)
+setup(ext_modules=ext_modules)
